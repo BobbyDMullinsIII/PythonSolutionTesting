dd051a71259fad0f64dcf6da0903e44383240f6b
BobbyDMullinsIII <bobbymullins20000@gmail.com>
2023-03-16 23:01:30 -0400

    Update Problem1.py

--- Problem1.py
+++ Problem1.py
@@ -7,14 +7,13 @@ class Problem1:
         pass
     
     @staticmethod
-    def twoSumBruteForce(self, nums: List[int], target: int) -> List[int]:
-        
+    def twoSumBruteForce(self, nums: List[int], target: int) -> List[int]:     
         for i in range(len(nums)):
             for j in range(len(nums)):
                 if i != j:
                     if nums[i] + nums[j] == target:
                         return [i, j]
     
-    
+    @staticmethod 
     def twoSumEfficient(self, nums: List[int], target: int) -> List[int]:
         return 
