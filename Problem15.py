23095d16e57a856b53dc3fc231b29b2211732dab
BobbyDMullinsIII <bobbymullins20000@gmail.com>
2023-05-12 02:26:42 -0400

    Add skeleton classes for problem 15 and 16

---
+++ Problem15.py
@@ -0,0 +1,9 @@
+
+
+
+from typing import List
+
+class Problem15:
+    
+    def __init__(self) -> None:
+        pass
---
+++ Problem16.py
@@ -0,0 +1,9 @@
+
+
+
+from typing import List
+
+class Problem16:
+
+    def __init__(self) -> None:
+        pass
